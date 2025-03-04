fccf8ed1e25a9b3e1fcafd0acdc0249c97545947
DigitalElevation <geral@digitalelevation.com>
2025-03-04 13:34:12 +0000

    Changed app name

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -107,7 +107,7 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Artha AI")
 if WEBUI_NAME != "Open WebUI":
     WEBUI_NAME += " (Open WebUI)"
 
